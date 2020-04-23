303e02eb6f6f8c4242c16088508aaa6307dcc195
Viviane Miranda <vivianehfm@gmail.com>
2020-04-23 16:39:47 -0300

    exemplos de remove e removeChild

--- Aula - 2/exercicios/exercicio 3/script.js
+++ Aula - 2/exercicios/exercicio 3/script.js
@@ -20,44 +20,59 @@
 
 */
 
-const arrayLivros = [
-  {
-    titulo: "Doze contos peregrinos",
-    autor: 'Gabriel García Marquez'
-  },
-  {
-    titulo: "Amor em tempos de cólera",
-    autor: 'Gabriel García Marquez'
-  },
-  {
-    titulo: "Cem anos de solidão",
-    autor: 'Gabriel García Marquez'
-  }
+// const arrayLivros = [
+//   {
+//     titulo: "Doze contos peregrinos",
+//     autor: 'Gabriel García Marquez',
+//     capa: "http://imagens.lelivros.love/2016/09/Baixar-Livro-Doze-Contos-Peregrinos-Gabriel-Garc%C3%ADa-M%C3%A1rquez-em-PDF-ePub-e-Mobi-ou-ler-online-370x574.jpg"
+//   },
+//   {
+//     titulo: "Amor nos tempos do cólera",
+//     autor: 'Gabriel García Marquez',
+//     capa: "https://http2.mlstatic.com/livro-o-amor-nos-tempos-do-colera-D_NQ_NP_861515-MLB26557863387_122017-O.webp"
+//   },
+//   {
+//     titulo: "Cem anos de solidão",
+//     autor: 'Gabriel García Marquez',
+//     capa: "https://http2.mlstatic.com/livro-cem-anos-de-solido-gabriel-garcia-marquez-1-edico-D_NQ_NP_641669-MLB31012879859_062019-F.jpg"
+//   }
 
-]
-//acessando o body do html com query selector
+// ]
+// //acessando o body do html com query selector
+// const body = document.querySelector('body')
+
+// for (i = 0; i < arrayLivros.length; i++) {
+//   //criar uma div dentro do body
+//   const livroContainer = document.createElement('div')
+  
+//   //criando titulo, paragrafo pro autor e capa, e add seu conteudo
+//   const tituloLivro = document.createElement('h2')
+//   tituloLivro.innerText = `Titulo: ${arrayLivros[i].titulo}`
+
+//   const paragrafoAutor = document.createElement('p')
+//   paragrafoAutor.innerText = `Autor: ${arrayLivros[i].autor}`
+
+//   const imagemCapa = document.createElement ('img')
+//   imagemCapa.classList.add('capas')
+//   imagemCapa.src = arrayLivros[i].capa
+
+
+
+//   //inserindo a div dentro do body
+//   body.appendChild(livroContainer)
+
+//   //inserindo o h2 e paragrafo autor dentro da div
+//   livroContainer.appendChild (tituloLivro)
+//   livroContainer.appendChild (paragrafoAutor)
+//   livroContainer.appendChild (imagemCapa)
+
+//   console.log (livroContainer)
+
+// }
+
+function removerElemento(){
+const h3 = document.querySelector('h3')
 const body = document.querySelector('body')
 
-for (i = 0; i < arrayLivros.length; i++) {
-  //criar uma div dentro do body
-  const livroContainer = document.createElement('div')
-  
-  //criando titulo e paragrafo pro autor e add seu conteudo
-  const tituloLivro = document.createElement('h2')
-  tituloLivro.innerText = `Titulo: ${arrayLivros[i].titulo}`
-
-  const paragrafoAutor = document.createElement('p')
-  paragrafoAutor.innerText = `Autor: ${arrayLivros[i].autor}`
-
-
-  //inserindo a div dentro do body
-  body.appendChild(livroContainer)
-
-  //inserindo o h2 e paragrafo autor dentro da div
-  livroContainer.appendChild (tituloLivro)
-  livroContainer.appendChild (paragrafoAutor)
-
-  console.log (livroContainer)
-
+body.removeChild(h3)
 }
-
