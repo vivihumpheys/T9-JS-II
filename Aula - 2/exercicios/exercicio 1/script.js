7124348f7f16484435b239ace1cf24dcebdf74f3
Viviane Miranda <vivianehfm@gmail.com>
2020-04-23 14:34:40 -0300

    resolução ecs 1 e 3

--- Aula - 2/exercicios/exercicio 1/script.js
+++ Aula - 2/exercicios/exercicio 1/script.js
@@ -8,9 +8,18 @@
   
 */
 
-function insertion(el,id,text){
-  //seu código aqui
+
+function insereElementoTexto(elemento, id, texto) {
+  const elementoNovo = document.createElement(elemento)
+  elementoNovo.innerText = texto
+  const elementoExistente = document.querySelector(id)
+  elementoExistente.appendChild(elementoNovo)
+
 }
- 
+
 //a função de criar um parágrafo dentro do elemento com ID "conteudo" e com o texto "Fui criado com Javascript"
-insertion('p', "conteudo", "Fui criado com Javascript")
+insereElementoTexto('p', "#container", "Fui criado com Javascript")
+
+insereElementoTexto('h1', '#outradiv', "Outro textinho bolado criado pela func")
+
+insereElementoTexto('strong', '#maisoutradiv', ' ele tá fortinho ele')
--- Aula - 2/exercicios/exercicio 3/script.js
+++ Aula - 2/exercicios/exercicio 3/script.js
@@ -20,5 +20,44 @@
 
 */
 
-const booksArray = []
+const arrayLivros = [
+  {
+    titulo: "Doze contos peregrinos",
+    autor: 'Gabriel García Marquez'
+  },
+  {
+    titulo: "Amor em tempos de cólera",
+    autor: 'Gabriel García Marquez'
+  },
+  {
+    titulo: "Cem anos de solidão",
+    autor: 'Gabriel García Marquez'
+  }
+
+]
+//acessando o body do html com query selector
+const body = document.querySelector('body')
+
+for (i = 0; i < arrayLivros.length; i++) {
+  //criar uma div dentro do body
+  const livroContainer = document.createElement('div')
+  
+  //criando titulo e paragrafo pro autor e add seu conteudo
+  const tituloLivro = document.createElement('h2')
+  tituloLivro.innerText = `Titulo: ${arrayLivros[i].titulo}`
+
+  const paragrafoAutor = document.createElement('p')
+  paragrafoAutor.innerText = `Autor: ${arrayLivros[i].autor}`
+
+
+  //inserindo a div dentro do body
+  body.appendChild(livroContainer)
+
+  //inserindo o h2 e paragrafo autor dentro da div
+  livroContainer.appendChild (tituloLivro)
+  livroContainer.appendChild (paragrafoAutor)
+
+  console.log (livroContainer)
+
+}
 
